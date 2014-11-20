0463d5707ae4232d1e67aa3a717e9e67011dc173
derekdowling <me@derekdowling.com>
2014-11-20 14:25:55 -0700

    Adding various scripts

---
+++ solo.rb
@@ -0,0 +1,17 @@
+
+
+# Chef Solo Config File
+
+#
+
+# This file is provided as an example of /etc/chef/solo.rb.
+
+
+
+log_level          :info
+
+log_location       STDOUT
+
+file_cache_path    "/var/chef/cookbooks"
+
+role_path          "/var/chef/cookbooks/esruby"
