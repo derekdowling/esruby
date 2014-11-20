ffbf8f65f47af61668a6ad77f5e5f6c2bdce1d64
derekdowling <me@derekdowling.com>
2014-11-20 16:53:09 -0700

    Fixing double installs, adding chef-solo-search

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,12 @@
 #
-# Cookbook Name:: ES-Ruby
+# Cookbook Name:: esruby
 # Recipe:: default
 #
-# Copyright (c) 2014 The Authors, All Rights Reserved.
+# Copyright (c) 2014 Derek Dowling
 #
 
-# FUTURE ELASTICSEARCH SERVER
-# Until we start provisioning different types of production servers, this will be
-# sufficient. Otherwise, these cookbooks are better suited to specific run lists
-# that are determined by the role played by node.
+# Basic ES Setup
+
 include_recipe "java"
 include_recipe "elasticsearch::default"
 include_recipe "elasticsearch::search_discovery"
@@ -17,6 +15,7 @@ include_recipe "elasticsearch::search_discovery"
 include_recipe "ufw"
 include_recipe "firewall"
 
+# Not Secure At All, But Convenient
 firewall_rule "es" do
     protocol :tcp
     ports [9200, 9300]
